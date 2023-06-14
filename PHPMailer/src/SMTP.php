93eb58dbd622c3c954b6988e24ff080dcca0b1e0
raul-amaral <ramar.lda.2022@gmail.com>
2023-06-15 01:55:10 +0900

    Hadia no hatama file JS no CSS balun

--- PHPMailer/src/SMTP.php
+++ PHPMailer/src/SMTP.php
@@ -23,22 +23,25 @@ try {
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    // $mail->setFrom('from@example.com', 'Mailer');
+    $mail->setFrom('campoalor@dadus-suku.online', 'SISCA - Sistema Informasaun Suku Campo Alor');
+    // $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->addAddress($_POST['email'], $_POST['name']);
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('$_POST$email', 'Information'); 
+    $mail->addReplyTo( $_POST['email']);
+    $mail->addCC('marlora010121@gmail.com');
+    $mail->addBCC('maria.viana.belo@gmail.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'SISCA - Sistema Informasaun Suku Campo Alor';
+    $mail->Body    = 'Obrigado! Ita rejista ona iha <b>Dadus Suku Campo Alor</b>';
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
